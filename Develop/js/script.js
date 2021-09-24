3f71e33a4dd07761fc263180ad7c38b7028b3bce
beamchristian <beamchristian@yahoo.com>
2021-09-24 19:17:57 -0400

    saveBtn on click

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -3,14 +3,17 @@
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
 //<p id="currentDay" is the element that will display the current day//
-// let currentDayDisplay = document.querySelector('#currentDay');
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); // moment.js
-    
-const loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
 
-}
-
+// assign click listener for save.Btn 
+$(".saveBtn").on("click", function () {
+    console.log(this);
+    // search through siblings of 'this' and capture the value of '.description'
+    let text = $(this).siblings(".description").val();  
+    // parent html element 
+    let time = $(this).parent().attr("id");
+    localStorage.setItem(time, text);
+})
 
 // WHEN I view the timeblocks for that day
 // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
