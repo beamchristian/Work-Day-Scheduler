56589debdb1d1cb5a999e0c2474e26865a3397b5
beamchristian <beamchristian@yahoo.com>
2021-09-24 20:53:47 -0400

    loading local storage correctly and displaying classes according to time

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,30 +1,58 @@
 "use strict";
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-//<p id="currentDay" is the element that will display the current day//
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY")); // moment.js
 
 // assign click listener for save.Btn 
 $(".saveBtn").on("click", function () {
-    console.log(this);
+    
     // search through siblings of 'this' and capture the value of '.description'
     let text = $(this).siblings(".description").val();  
     // parent html element 
     let time = $(this).parent().attr("id");
     localStorage.setItem(time, text);
 })
+// load saved data if any from LocalStorage for every hour
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
-// WHEN I view the timeblocks for that day
-// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
 
-// WHEN I click into a timeblock
-// THEN I can enter an event
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
+function timeTracker() {
+    // get the current hour
+    let currentTime = moment().hour();
+    
+
+    // loop over time blocks
+    $(".time-block").each(function () {
+        let timeBlockHour = parseInt($(this).attr("id").split("hour")[1]);
+        console.log(timeBlockHour, currentTime)
+    
+
+    // check current time and assign css to indicated if time is past, present or future
+    if (timeBlockHour < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        
+    } else if (timeBlockHour === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    }
+
+    })
+
+}
+timeTracker();
 
 
-// WHEN I refresh the page
-
-
-// THEN the saved events persist
